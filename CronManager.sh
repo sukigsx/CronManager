3b0c9b3efd928992151b7f0091ca1165cf00a2f9
sukigsx <sukigsx.mbsistemas@gmail.com>
2026-02-18 10:12:36 +0100

    Update CronManager.sh

--- CronManager.sh
+++ CronManager.sh
@@ -595,7 +595,7 @@ borrar_tarea() {
 
     CRON_CONTENT=$(crontab -l 2>/dev/null | grep -v '^\s*$' | grep -v '^#' | sed 's/^/   /')
     if [[ -z "$CRON_CONTENT" ]]; then
-        echo -e "${rojo} No hay tareas programadas en el crontab de${borra_colores} $(whoami)"; sleep 2
+        echo -e "${rojo} No hay tareas programadas en el crontab de${borra_colores} $(whoami)"; sleep 3
     else
         #crontab -l 2>/dev/null > $CRON_TMP || { echo -e "${rojo}No hay tareas.${borra_colores}"; return; }
         crontab -l 2>/dev/null | grep -v '^\s*#' | grep -v '^\s*$' > "$CRON_TMP"
